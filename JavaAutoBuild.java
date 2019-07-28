9bb3f9ddeed962e07e1a0aaf16bf1e27fbe4b1c8
root <root@ip-172-31-40-246.us-east-2.compute.internal>
2019-07-28 15:19:32 +0000

    changes done

--- JavaAutoBuild.java
+++ JavaAutoBuild.java
@@ -1,7 +1,8 @@
 public class JavaAutoBuild {
 
   public static void main(String[] args) {
-    System.out.println("Hallo Docker Workshop!");
+    System.out.println("Hallo Docker Workshop! poc github-docker");
     System.out.println("Ich wurde am Docker Donnerstag automatisch erstellt!");
   }
+
 }
